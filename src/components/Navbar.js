7ed7880f51e9606faa4d024f5540f410fb4668e8
himal999 <himal.official99@gmail.com>
2022-05-25 16:39:26 +0530

    complete nav bar with responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,25 @@ import {BsFillHouseFill} from 'react-icons/bs';
 
 import './Navbar.css';
 const Navbar = ()=>{
+    const [click,setClick] = useState(false);
+    const hadleClick = ()=> setClick(!click);
     return(
         <div className='navbar'>
-            <div className='contaainer'>
+            <div className='container'>
                 <h1><span><BsFillHouseFill/>Real</span>Estate</h1>
+                <button className='btn'>Sign In</button>
+                <ul className={click ? 'nav-menu active': 'nav-menu'}>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Search</a></li>
+                    <li><a href='#'>About</a></li>
+                    <li><a href='#'>Contact</a></li>
+                </ul>
+                <div className='hamburger' onClick={hadleClick}>
+                    {
+                        click ? <FaRegTimesCircle className='icon'/>: <HiOutlineMenuAlt4 className='icon'/>
+                    }
+                 
+                </div>
             </div>
         </div>
     );
