51aba1e9c5cc34498b8c1e2c13e37e9713e16cb4
himal999 <himal.official99@gmail.com>
2022-05-25 17:42:04 +0530

    Hero section

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,27 @@
+import react from 'react'
+import {AiOutlineSearch} from 'react-icons/ai';
+import './Hero.css';
+const Hero = ()=>{
+    return(
+        <div className='hero'>
+            <div className='content'>
+                <h1>Find the Amazing Places</h1>
+                <p className='search-text'>Search the largest selection of luxury high-rise apartments,multi-family homes and luxury homes</p>
+                <form className='search'>
+                    <div>
+                        <input type='text' placeholder='Enter keyword...'/>
+                    </div>
+                    <div className='radio'>
+                        <input type='radio' checked/>
+                        <lable>Buy</lable>
+                        <input type='radio' />
+                        <lable>Rent</lable>
+                        <button type="submit"><AiOutlineSearch className="icon"/></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
